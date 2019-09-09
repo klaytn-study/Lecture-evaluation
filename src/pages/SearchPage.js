21d83ec9f8805940565497895ff8b2efb64d875c
ChoiEunji0114 <cheunji8209@naver.com>
2019-09-09 22:23:23 +0900

    Update LectureList

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -34,11 +34,54 @@ class SearchBar extends Component {
     }
 }
 
+class LectureList extends Component{
+    renderClass({lecture, list}){
+      return(
+          <tr key={lecture.id}>
+              <td>{lecture.name}</td>
+              <td>{list[0].main.professor}</td>
+              <td>{list[0].main.campus}</td>
+  
+          </tr>
+      );
+      }
+    handlerError(){
+      if(this.props.error){
+        return(
+            <div className="alert alert-danger" role="alert">
+                {this.props.error.message}
+            </div>
+        );
+      }
+    }
+  
+    render(){
+      return(
+        <div className='LectureList'>
+          {this.handlerError()}
+          <table className='table table-hover'></table>
+            <thead>
+              <tr>
+                <th>Campus</th>
+                <th>Lecture</th>
+                <th>Professor</th>
+              </tr>
+            </thead>
+            <tbody>
+  
+            </tbody>
+        </div>
+      );
+    }
+  }
+
+
 class SearchPage extends Component {
     render(){
         return (
             <div>
                 <SearchBar/>
+                <LectureList/>
             </div>
         );
     }
