edbc17a93630941724522f19e4d02d5a58f45b1d
Uranel <pharanula@gmail.com>
2019-09-17 09:31:55 +0900

    Update SignupForm.js
    updated button disabled

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { cav } from 'klaytn/caver'
 import Input from 'components/Input'
 import Button from 'components/Button'
-// import { sendEmail } from 'utils/auth'
+//import { sendEmail } from '../utils/auth'
 
 import './SignupForm.scss'
 
@@ -12,6 +12,7 @@ class SignupForm extends Component {
     email: '',
     emailValid: 0, // 발송되는 코드
     userInput: 0, // 유저가 input하는 코드
+    disabledButton: true,
   }
 
   // 최종적으로 실행되어야 하는 것
@@ -50,8 +51,14 @@ class SignupForm extends Component {
     if (parseInt(this.state.number) === this.state.answer) {
       // disabled = "false"
       alert('인증되셨습니다')
+      this.setState({
+        disabledButton: false,
+      })
     } else{
       alert('인증코드를 다시 확인해주세요')
+      this.setState({
+        disabledButton: false,
+      })
     }
   }
 
@@ -112,7 +119,7 @@ class SignupForm extends Component {
                 className="SignupForm__Final"
                 title="PK생성"
                 onClick={this.generatePrivateKey}
-                disabled="true"
+                disabled={this.state.disabledButton}
               />
           </div>
         </div>
