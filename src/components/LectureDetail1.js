c12fe1c41ec275c4569afafb5a1800ca84ba87ad
Sunghee <630sunghee@gmail.com>
2019-09-27 01:05:20 +0900

    Update LectureDetail1.js

--- src/components/LectureDetail1.js
+++ src/components/LectureDetail1.js
@@ -6,38 +6,46 @@ import moment from 'moment'
 import ui from 'utils/ui'
 import EvaluationDetail from './EvaluationDetail'
 
-const DetailLec = ({ items, lectureName, lecturePro, courseId }) => (
-  <Table className="form-control-lecture" bordered hover >
-    <thead>
-      <tr>
-        <th>Index</th>
-        <th>Tittle</th>
-        <th>Date</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        items.map(({ title, timestamp }, index) => (
-          <tr
-            key={index}
-            onClick={() => ui.showModal({
-              header: 'EvaluationDetail',
-              content: <EvaluationDetail
-                id={courseId}
-                evalId={index}
-                lecName={lectureName}
-                professor={lecturePro}
-              />,
-            })}
-          >
-            <td>{index}</td>
-            <td>{title}</td>
-            <td>{moment(timestamp * 1000).format('YYYY-MM-DD HH:mm:ss')}</td>
-          </tr>
-        ))
-      }
-    </tbody>
-  </Table>
-)
+class DetailLec extends Component {
+  render() {
+    const { items, lectureName, lecturePro, courseId } = this.props
+    if (this.props.items.length == 0) {
+      return <div> 등록된 강의평가가 없습니다. </div>;
+    }
+    return (
+      <Table className="form-control-lecture" bordered hover >
+      <thead>
+        <tr>
+          <th>Index</th>
+          <th>Tittle</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          items.map(({ title, timestamp }, index) => (
+            <tr
+              key={index}
+              onClick={() => ui.showModal({
+                header: 'EvaluationDetail',
+                content: <EvaluationDetail
+                  id={courseId}
+                  evalId={index}
+                  lecName={lectureName}
+                  professor={lecturePro}
+                />,
+              })}
+            >
+              <td>{index}</td>
+              <td>{title}</td>
+              <td>{moment(timestamp * 1000).format('YYYY-MM-DD HH:mm:ss')}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </Table>
+    );
+  }
+}
 
 export default DetailLec
