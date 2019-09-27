a1ad5fcc881f7490af9ae26dc956fed8c8674c55
Uranel <pharanula@gmail.com>
2019-09-27 15:35:04 +0900

    Evaluation good/bad complete(except css)

--- src/components/LectureDetail1.js
+++ src/components/LectureDetail1.js
@@ -19,11 +19,12 @@ class DetailLec extends Component {
           <th>Index</th>
           <th>Tittle</th>
           <th>Date</th>
+          <th>평가</th>
         </tr>
       </thead>
       <tbody>
         {
-          items.map(({ title, timestamp }, index) => (
+          items.map(({ title, timestamp, good, bad }, index) => (
             <tr
               key={index}
               onClick={() => ui.showModal({
@@ -39,6 +40,10 @@ class DetailLec extends Component {
               <td>{index}</td>
               <td>{title}</td>
               <td>{moment(timestamp * 1000).format('YYYY-MM-DD HH:mm:ss')}</td>
+              <td>
+                이 평가 유용함!!!: {good}<br/>
+                이 평가 쓰레기임ㅠㅠㅠ: {bad}
+              </td>
             </tr>
           ))
         }
